cf0205626d3907f07a4012a0da993d30e4ab25ab
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-14 15:38:11 -0500

    feat: adding all routes to main server

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const cors = require('cors');
 const morgan = require('morgan');
 const port = process.env.PORT || 3000;
 const usuarioRoutes = require('./routes/usuario.routes.js');
+const routes = require('./routes/ruta.routes.js');
 const authRoutes = require('./routes/auth.routes.js');
+const recursos = require('./routes/recurso.routes.js');
+const diarios = require('./routes/diario.routes.js');
 connectDB();
 
 app.use(cors());
@@ -16,6 +19,9 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/rutas', routes);
+app.use('/api/recursos', recursos);
+app.use('/api/diarios', diarios)
 app.use('/auth', authRoutes);
 app.listen(port, () => {
   console.log('escuchando en el puerto ', port);
