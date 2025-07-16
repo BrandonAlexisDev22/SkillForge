0736332bd28ddf6cd48ade7531f751272004e684
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-16 00:04:10 -0500

    feat: upd App.tsx with react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,22 @@
-import Header from "@/components/Header";
-function App() {
-  return <Header />
+import { Routes, Route } from 'react-router-dom';
+import Register from '@/components/Register';
+import Header from '@/components/Header';
+import LoginForm from '@/components/Login';
+import Footer from './components/Footer';
 
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="container-fluid min-vh-100 w-100 d-flex justify-content-center align-items-center">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/registro" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }
-export default App
+
+export default App;
