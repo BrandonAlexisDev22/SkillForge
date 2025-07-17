5be695536e611f80bee5b1f1da0f8d44573d39fd
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-17 14:38:01 -0500

    update: updating App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,16 @@
-import Home from '@/pages/Home.tsx'
+import Home from '@/pages/Home.tsx';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-    <Home />
+      <Routes>
+        <Route path='/inicio' element={<Home />} />
+        <Route path="/inicio-sesion" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+      </Routes>
     </>
   );
 }
 export default App;
-
-// import { Routes, Route } from 'react-router-dom';
-// import Register from '@/components/Register';
-// import Header from '@/components/Header';
-// import LoginForm from '@/components/Login';
-// import Footer from './components/Footer';
-  //  <>
-  //     <Header />
-  //     <main className="container-fluid min-vh-100 w-100 d-flex justify-content-center align-items-center">
-  //       <Routes>
-  //         <Route path="/" element={<LoginForm />} />
-  //         <Route path="/registro" element={<Register />} />
-  //       </Routes>
-  //     </main>
-  //     <Footer />
-  //   </>
