3cfb65c716e74f32e5533c5d399e28f9fb0e0acc
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-16 19:36:38 -0500

    upd: change structure temporaly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import Register from '@/components/Register';
-import Header from '@/components/Header';
-import LoginForm from '@/components/Login';
-import Footer from './components/Footer';
-
+import Home from '@/pages/Home.tsx'
 function App() {
   return (
     <>
-      <Header />
-      <main className="container-fluid min-vh-100 w-100 d-flex justify-content-center align-items-center">
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/registro" element={<Register />} />
-        </Routes>
-      </main>
-      <Footer />
+    <Home />
     </>
   );
 }
-
 export default App;
+
+// import { Routes, Route } from 'react-router-dom';
+// import Register from '@/components/Register';
+// import Header from '@/components/Header';
+// import LoginForm from '@/components/Login';
+// import Footer from './components/Footer';
+  //  <>
+  //     <Header />
+  //     <main className="container-fluid min-vh-100 w-100 d-flex justify-content-center align-items-center">
+  //       <Routes>
+  //         <Route path="/" element={<LoginForm />} />
+  //         <Route path="/registro" element={<Register />} />
+  //       </Routes>
+  //     </main>
+  //     <Footer />
+  //   </>
