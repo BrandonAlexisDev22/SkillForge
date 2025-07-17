f29986aeb1e272592874ab7a64ed6ec9f1738405
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-17 15:09:45 -0500

    update: updating App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import Home from '@/pages/Home.tsx';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Nosotros from './pages/Nosotros';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path='/inicio' element={<Home />} />
         <Route path="/inicio-sesion" element={<Login />} />
         <Route path="/registro" element={<Register />} />
+        <Route path="/nosotros" element={<Nosotros />} />
       </Routes>
     </>
   );
