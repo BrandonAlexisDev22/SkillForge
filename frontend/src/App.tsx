06ff9cc84b5d726130fbd604e78a956011ac2d38
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-08-13 21:44:11 -0500

    feat: add dashboard route to application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import Home from '@/pages/Home.tsx';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Nosotros from './pages/Nosotros';
+import Dashboard from './pages/dashboardUser';
 import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <>
@@ -11,6 +13,7 @@ function App() {
         <Route path="/inicio-sesion" element={<Login />} />
         <Route path="/registro" element={<Register />} />
         <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
